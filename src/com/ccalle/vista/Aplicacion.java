e1afc9d08e17d5387fc83c5b7fe962e5898ff5d6
Alex <alex_viteri3@hotmail.com>
2018-11-12 23:28:43 -0500

    NixonViteri

--- src/com/ccalle/vista/Aplicacion.java
+++ src/com/ccalle/vista/Aplicacion.java
@@ -14,6 +14,7 @@ public class Aplicacion {
         System.out.println("Hola Mundo");
         System.out.println("Hola Mundo Esteban Ayo");
         System.out.println("Hola Mundo Cristian Calle");
+        System.out.println("Hola mundo Viteri Nixon");
     }
     
 }
